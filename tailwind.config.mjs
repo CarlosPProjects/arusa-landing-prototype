817e2058283b7631bc05bce7490db0432b9a9ca9
CarlosPProjects <cg9822@gmail.com>
2024-11-25 22:17:21 +0100

    Add Tailwind CSS integration and global styles

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: 'hsl(var(--primary))',
+				accent: 'hsl(var(--accent))',
+				text: 'hsl(var(--text))',
+				border: 'hsl(var(--border))',
+				background: 'hsl(var(--background))',
+				dark: 'hsl(var(--dark))',
+			},
+			fontFamily: {
+				gt: ['GTAmericaExpended', 'sans-serif'],
+				sans: ['BasicCommercial', 'Helvetica' ,'sans-serif'],
+				serif: ['NeueWorldCondensed', 'Century', 'serif'],
+			}
+		},
+	},
+	plugins: [],
+}
